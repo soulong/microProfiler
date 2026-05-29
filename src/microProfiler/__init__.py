7694641cee51b99e7769106d98c45d18b769ff2f
Hao He <haohe90@gmail.com>
2026-05-29 16:20:43 +0800

    new build tool

--- src/microProfiler/__init__.py
+++ src/microProfiler/__init__.py
@@ -1,16 +1,12 @@
 """microProfiler: microscopy image preprocessing, segmentation, and profiling."""
 
-from importlib.metadata import PackageNotFoundError, version
-
 from microProfiler.io.dataset import ImageDataset
 from microProfiler.io.database import Database
 
-try:
-    __version__ = version("microProfiler")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
+__version__ = "0.9.6"
 
 __all__ = [
+    "__version__",
     "ImageDataset",
     "Database",
 ]
